6ccc86d37e26aef3c0bb60b709dd83e46099916d
Saad1430 <chalbhaag.420@gmail.com>
2023-09-21 16:58:24 +0500

    db connection successful

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
+const { Schema } = mongoose;
 
 // Middlewares
 app.use(express.json());
@@ -12,27 +13,55 @@ app.use(morgan('tiny'));
 require('dotenv/config');
 const api = process.env.API_URL;
 
+// Schemas / Models / Collection
+const productSchema = new Schema({
+	name: {
+    type: String,
+    required: true
+  },
+	image: String,
+	countInStock: {
+    type: Number,
+    required: true
+  },
+});
+
+
+const Product = mongoose.model('Product', productSchema);
+
+// APIs
 app.get(`${api}/`, (req, res)=>{
   res.send('Hello API');
 });
 
-app.get(`${api}/products`, (req, res)=>{
-  const product = {
-    id: 1,
-    name: 'Poco x3 pro',
-    image: 'http://abc.xyz/123',
-  };
-  res.send(product);
+app.get(`${api}/products`, async (req, res)=>{
+  const productList = await Product.find();
+  if (!productList) {
+    res.status(500).json({success:false});
+  }
+  res.send(productList);
 });
 
 app.post(`${api}/products`, (req, res)=>{
-  const newProduct = req.body;
-  console.log(newProduct);
-  res.send(newProduct);
+  const newProduct = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock,
+  })
+  newProduct.save().then((createdProduct=>{
+    res.status(201).json(createdProduct)
+  })).catch((err=>{
+    res.status(500).json({
+      error: err,
+      success: false,
+    })
+  }))
 });
 
-
-mongoose.connect(process.env.DB)
+// Connection string
+mongoose.connect(process.env.DB,{
+  dbName: 'ecommerce'
+})
 .then(()=>{
   console.log('Database connection is ready...!');
 })
@@ -40,6 +69,7 @@ mongoose.connect(process.env.DB)
   console.error('Error: ' + err);
 })
 
+// App starter config
 app.listen(port, '0.0.0.0', ()=>{
   console.log(`Server started. Go to http://localhost:${port}${api}`);
 });
